b8c05f45d5e9179467a6578e0147c10c2bc838dd
Alexey Subbotin <alesu1543@gmail.com>
2023-03-09 10:48:01 +0300

    nw

--- add.sql
+++ add.sql
@@ -6,12 +6,18 @@ SET time_zone = "+00:00";
 CREATE DATABASE IF NOT EXISTS test;
 
 CREATE TABLE IF NOT EXISTS myarttable (
-  `id` int(11) NOT NULL,
-  `text` text NOT NULL,
-  `description` text NOT NULL,
-  `keywords` text NOT NULL
+  id int(11) NOT NULL,
+  text text NOT NULL,
+  description text NOT NULL,
+  keywords text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=cp1251;
 
+ALTER TABLE myarttable
+  ADD PRIMARY KEY (`id`);
+  
+ALTER TABLE myarttable
+  MODIFY id int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  
 INSERT INTO myarttable (text, description, keywords) VALUES
 ('Февраль', '128', 'Сидоров В.Н.'),
 ('Март', '520', 'Осипов А.К.'),
